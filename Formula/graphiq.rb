082c2f90169221f2ff4fb399dab490e514d32c22
Avery Felts <alexmondello@Averys-MacBook-Air.local>
2026-04-16 16:38:07 -0600

    graphiq v0.1.0

--- Formula/graphiq.rb
+++ Formula/graphiq.rb
@@ -7,17 +7,17 @@ class Graphiq < Formula
   on_macos do
     on_arm do
       url "https://github.com/aaf2tbz/graphiq/releases/download/v#{version}/graphiq-aarch64-apple-darwin.tar.gz"
-      sha256 "REPLACE_aarch64_SHA256"
+      sha256 "1d20637dcb58725baace906a50b835e67377ce87155c647d835a2207203af5fc"
     end
     on_intel do
       url "https://github.com/aaf2tbz/graphiq/releases/download/v#{version}/graphiq-x86_64-apple-darwin.tar.gz"
-      sha256 "REPLACE_x86_64_SHA256"
+      sha256 "8960f6573dbb206799b5cd630e4631d97d25e66df25844b98156d717e9312774"
     end
   end
 
   on_linux do
     url "https://github.com/aaf2tbz/graphiq/releases/download/v#{version}/graphiq-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "REPLACE_LINUX_SHA256"
+    sha256 "ef7164cdf08abe28ad1fb82d025ab50984255a30a7b1afca61f54be5b2cb1475"
   end
 
   def install
