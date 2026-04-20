cd00dfada22616143fd320dd076bbf5dbf70fbe5
Avery Felts <alexmondello@Averys-MacBook-Air.local>
2026-04-20 13:22:26 -0600

    graphiq v4.2

--- Formula/graphiq.rb
+++ Formula/graphiq.rb
@@ -1,23 +1,23 @@
 class Graphiq < Formula
   desc "Structural code intelligence — unified pipeline with BM25, spectral diffusion, holographic matching"
   homepage "https://github.com/aaf2tbz/graphiq"
-  version "0.4.1"
+  version "4.2"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/aaf2tbz/graphiq/releases/download/v#{version}/graphiq-aarch64-apple-darwin.tar.gz"
-      sha256 "e05697268caebe2dbea0218dc07d883bf36a988298b64e8de30f46f1ede29755"
+      sha256 "4abd2f14a04909366ea28f19eb6632c8304268f2673b4bc863158d5606bab5d1"
     end
     on_intel do
       url "https://github.com/aaf2tbz/graphiq/releases/download/v#{version}/graphiq-x86_64-apple-darwin.tar.gz"
-      sha256 "edfd0f270cad79bc4520397391f9ab3e5e86ad8115cb11216bb0c5be51c931a0"
+      sha256 "fe562660da15543b430180e91aee90138d9db0a188b3701096db1acde016440b"
     end
   end
 
   on_linux do
     url "https://github.com/aaf2tbz/graphiq/releases/download/v#{version}/graphiq-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "826048bef2d4484d6dd85f2a10a64cc6a1c2218ae0a90dd1e8c8d219f04474d7"
+    sha256 "a5c38ec34a0d27530130b6c9f2d456c131e985b827fff90aab530035c793bb15"
   end
 
   def install
